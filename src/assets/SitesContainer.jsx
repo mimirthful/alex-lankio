3b019bf93da8ec2cdd55b6733fd1400e40af276b
Alex Lankio <alex.lankio@proton.me>
2026-08-18 21:04:48 +0300

    chore: add a new link

--- src/assets/SitesContainer.jsx
+++ src/assets/SitesContainer.jsx
@@ -1,40 +1,51 @@
-import isoheikkila from "./images/isoheikkila.png"
-import pokedex from "./images/pokedex.png"
-import reflections from "./images/reflections.png"
-import InfoCard from "./InfoCard"
-export default function SitesContainer()
-{
-    return <div className="row justify-content-center my-3">
-            <h3 className="p-2 text-light"> Sites and apps</h3>
-            <InfoCard 
-              name="Iso-Heikkilä's Clubroom Booking" 
-              subtitle="React, css, JSON"
-              picture={isoheikkila} 
-              alt="Photo of the website"
-              description="Booking website for the Iso-Heikkilä's clubroom, created for the Annala-Iso-Heikkilä's tenants' committee."
-              linkAddress="https://mimirthful.github.io/room-booking/"
-              linkName="Check out"
-              />
+import isoheikkila from "./images/isoheikkila.png";
+import pokedex from "./images/pokedex.png";
+import reflections from "./images/reflections.png";
+import InfoCard from "./InfoCard";
+import Aurinkoone from "./images/aurinkoone.png";
+export default function SitesContainer() {
+  return (
+    <div className="row justify-content-center my-3">
+      <h3 className="p-2 text-light"> Sites and apps</h3>
+      <InfoCard
+        name="Aurinkoone"
+        subtitle="Python, wxPython"
+        picture={Aurinkoone}
+        alt="Photo of the app"
+        description="Python-based desktop app for Tampere locals, which shows weather and user's selected bus stops."
+        linkAddress="https://github.com/mimirthful/Aurinkoone"
+        linkName="Check out"
+      />
 
-            <InfoCard 
-              name="Pokédex" 
-              subtitle="React & Bootstrap 5"
-              picture={pokedex} 
-              alt="Photo of the website"
-              description="A small simple Pokédex site, which uses pokeapi to fetch information about the pokémon."
-              linkAddress="https://mimirthful.github.io/bootstrap-react-pokedex/"
-              linkName="Check out"
-              />
+      <InfoCard
+        name="Iso-Heikkilä's Clubroom Booking"
+        subtitle="React, css, JSON"
+        picture={isoheikkila}
+        alt="Photo of the website"
+        description="Booking website for the Iso-Heikkilä's clubroom, created for the Annala-Iso-Heikkilä's tenants' committee."
+        linkAddress="https://mimirthful.github.io/room-booking/"
+        linkName="Check out"
+      />
 
-            <InfoCard 
-              name="Reflections" 
-              subtitle="React, MUI & express, SQLite"
-              picture={reflections} 
-              alt="Photo of the app"
-              description="Ongoing project. It aims to be stoic journaling app using technologies common in fullstack."
-              linkAddress="https://github.com/mimirthful/reflections-journal/"
-              linkName="Check out"
-              />  
-    
-        </div>
+      <InfoCard
+        name="Pokédex"
+        subtitle="React & Bootstrap 5"
+        picture={pokedex}
+        alt="Photo of the website"
+        description="A small simple Pokédex site, which uses pokeapi to fetch information about the pokémon."
+        linkAddress="https://mimirthful.github.io/bootstrap-react-pokedex/"
+        linkName="Check out"
+      />
+
+      <InfoCard
+        name="Reflections"
+        subtitle="React, MUI & express, SQLite"
+        picture={reflections}
+        alt="Photo of the app"
+        description="Ongoing project. It aims to be stoic journaling app using technologies common in fullstack."
+        linkAddress="https://github.com/mimirthful/reflections-journal/"
+        linkName="Check out"
+      />
+    </div>
+  );
 }
